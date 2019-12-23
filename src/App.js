cc90be6f3df786000c93326b5b3558f3f5727947
Michael Peterson <michaeljoepeterson@gmail.com>
2019-12-23 11:45:19 -0700

    setup initial theme file and theme provider

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import LandingPage from './components/landing-page';
 import {Route, withRouter} from 'react-router-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import {theme} from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <Route exact path="/" component={LandingPage} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Route exact path="/" component={LandingPage} />
+      </div>
+    </ThemeProvider>
   );
 }
 
