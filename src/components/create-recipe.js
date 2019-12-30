cf96b005ae39481372045fc88616fb30de766824
Michael Peterson <michaeljoepeterson@gmail.com>
2019-12-30 15:40:56 -0700

    setup layout for recipe form

--- src/components/create-recipe.js
+++ src/components/create-recipe.js
@@ -5,25 +5,62 @@ import requiresLogin from '../HOC/requires-login';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 import Typography from '@material-ui/core/Typography';
 import './styles/containers.css';
 import './styles/forms.css';
+import { MenuItem } from '@material-ui/core';
 
 export class CreateRecipe extends React.Component{
-
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            step:1
+        };
+      }
+    //render step inputs based on step in state, to avoid js way of adding step
+    //also could do that for units so units could be added/removed
     render(){
         return(
             <div className="content-container">
                 <form>
-                    <Grid className="input-container-recipe" item xs={12}>
-                        <TextField fullWidth required id="title" label="Title" variant="outlined" />
-                    </Grid>
-                    <Grid className="input-container-recipe" item xs={12}>
-                        <TextField className='textfield-input' fullWidth required id="description" label="Description" variant="outlined" />
-                    </Grid>
-                    <Grid className="input-container-recipe" item xs={12}>
-                        <TextField required id="serving" label="Serving Size" variant="outlined" />
+                    <Grid container>
+                        <Grid className="input-container-recipe" item xs={12}>
+                            <TextField fullWidth required id="title" label="Title" variant="outlined" />
+                        </Grid>
+                        <Grid className="input-container-recipe" item xs={12}>
+                            <TextField required id="serving" label="Serving Size" variant="outlined" />
+                        </Grid>
+                        <Grid className="input-container-recipe" item xs={12}>
+                            <TextField fullWidth multiline required id="description" label="Description" variant="outlined" />
+                        </Grid>
+                        <Grid className="input-container-recipe"  alignItems='flex-end' item container spacing={2} xs={12} md={6}>
+                                <Grid item xs={12} md={4}>
+                                    <TextField className="input-spacing" fullWidth multiline required id="ingredient" label={"Ingredient " + this.state.step} variant="outlined" />
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <TextField className="input-spacing" fullWidth multiline required id="amount" label={"Amount " + this.state.step} variant="outlined" />
+                                </Grid>
+                                <Grid item xs={12} md={4}>
+                                    <InputLabel id="units">Units</InputLabel>
+                                    <Select id="units">
+                                        <MenuItem value={"cups"}>Cups</MenuItem>
+                                        <MenuItem value={"tbs"}>Tbs</MenuItem>
+                                        <MenuItem value={"ml"}>ml</MenuItem>
+                                        <MenuItem value={"litres"}>Litres</MenuItem>
+                                        <MenuItem value={"grams"}>grams</MenuItem>
+                                        <MenuItem value={"pieces"}>Pieces</MenuItem>
+                                    </Select>
+                                </Grid>
+                                <Grid xs={12}>
+                                    <Button className="input-spacing" variant="contained" color="primary">Add Ingredient</Button>
+                                </Grid>
+                            </Grid> 
+                        <Grid className="input-container-recipe" item xs={12} md={6}>
+                            <TextField fullWidth multiline required id="steps" label={"Step " + this.state.step} variant="outlined" />
+                            <Button className="input-spacing" variant="contained" color="primary">Add Step</Button>
+                        </Grid>
                     </Grid>
                 </form>
             </div>
